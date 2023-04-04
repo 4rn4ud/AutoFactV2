57dfa2131f92d9ce76c7bf8d36aed782498fc01b
4rn4ud <arnaud.lechat1@gmail.com>
2023-04-04 16:22:37 +0200

    base_project

---
+++ FormMenu.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace AutoFact2
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClientList = new System.Windows.Forms.Button();
+            this.btnClientUpdate = new System.Windows.Forms.Button();
+            this.btnClientCreate = new System.Windows.Forms.Button();
+            this.btnProductList = new System.Windows.Forms.Button();
+            this.btnProductUpdate = new System.Windows.Forms.Button();
+            this.btnProductCreate = new System.Windows.Forms.Button();
+            this.btnInvoiceList = new System.Windows.Forms.Button();
+            this.btnInvoiceUpdate = new System.Windows.Forms.Button();
+            this.btnInvoiceCreate = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClientList
+            // 
+            this.btnClientList.Location = new System.Drawing.Point(94, 99);
+            this.btnClientList.Name = "btnClientList";
+            this.btnClientList.Size = new System.Drawing.Size(110, 30);
+            this.btnClientList.TabIndex = 0;
+            this.btnClientList.Text = "Liste des clients";
+            this.btnClientList.UseVisualStyleBackColor = true;
+            this.btnClientList.Click += new System.EventHandler(this.btnClientList_Click);
+            // 
+            // btnClientUpdate
+            // 
+            this.btnClientUpdate.Location = new System.Drawing.Point(94, 159);
+            this.btnClientUpdate.Name = "btnClientUpdate";
+            this.btnClientUpdate.Size = new System.Drawing.Size(110, 30);
+            this.btnClientUpdate.TabIndex = 1;
+            this.btnClientUpdate.Text = "Modifier un client";
+            this.btnClientUpdate.UseVisualStyleBackColor = true;
+            this.btnClientUpdate.Click += new System.EventHandler(this.btnClientUpdate_Click);
+            // 
+            // btnClientCreate
+            // 
+            this.btnClientCreate.Location = new System.Drawing.Point(94, 218);
+            this.btnClientCreate.Name = "btnClientCreate";
+            this.btnClientCreate.Size = new System.Drawing.Size(110, 30);
+            this.btnClientCreate.TabIndex = 2;
+            this.btnClientCreate.Text = "Ajouter un client";
+            this.btnClientCreate.UseVisualStyleBackColor = true;
+            this.btnClientCreate.Click += new System.EventHandler(this.btnClientCreate_Click);
+            // 
+            // btnProductList
+            // 
+            this.btnProductList.Location = new System.Drawing.Point(94, 282);
+            this.btnProductList.Name = "btnProductList";
+            this.btnProductList.Size = new System.Drawing.Size(110, 30);
+            this.btnProductList.TabIndex = 3;
+            this.btnProductList.Text = "Liste des produits";
+            this.btnProductList.UseVisualStyleBackColor = true;
+            this.btnProductList.Click += new System.EventHandler(this.btnProductList_Click);
+            // 
+            // btnProductUpdate
+            // 
+            this.btnProductUpdate.Location = new System.Drawing.Point(94, 339);
+            this.btnProductUpdate.Name = "btnProductUpdate";
+            this.btnProductUpdate.Size = new System.Drawing.Size(110, 30);
+            this.btnProductUpdate.TabIndex = 4;
+            this.btnProductUpdate.Text = "Modifier un produit";
+            this.btnProductUpdate.UseVisualStyleBackColor = true;
+            this.btnProductUpdate.Click += new System.EventHandler(this.btnProductUpdate_Click);
+            // 
+            // btnProductCreate
+            // 
+            this.btnProductCreate.Location = new System.Drawing.Point(94, 407);
+            this.btnProductCreate.Name = "btnProductCreate";
+            this.btnProductCreate.Size = new System.Drawing.Size(110, 30);
+            this.btnProductCreate.TabIndex = 5;
+            this.btnProductCreate.Text = "Ajouter un produit";
+            this.btnProductCreate.UseVisualStyleBackColor = true;
+            this.btnProductCreate.Click += new System.EventHandler(this.btnProductCreate_Click);
+            // 
+            // btnInvoiceList
+            // 
+            this.btnInvoiceList.Location = new System.Drawing.Point(799, 99);
+            this.btnInvoiceList.Name = "btnInvoiceList";
+            this.btnInvoiceList.Size = new System.Drawing.Size(110, 30);
+            this.btnInvoiceList.TabIndex = 6;
+            this.btnInvoiceList.Text = "Liste des factures";
+            this.btnInvoiceList.UseVisualStyleBackColor = true;
+            this.btnInvoiceList.Click += new System.EventHandler(this.btnInvoiceList_Click);
+            // 
+            // btnInvoiceUpdate
+            // 
+            this.btnInvoiceUpdate.Location = new System.Drawing.Point(799, 159);
+            this.btnInvoiceUpdate.Name = "btnInvoiceUpdate";
+            this.btnInvoiceUpdate.Size = new System.Drawing.Size(110, 30);
+            this.btnInvoiceUpdate.TabIndex = 7;
+            this.btnInvoiceUpdate.Text = "Modifier une facture";
+            this.btnInvoiceUpdate.UseVisualStyleBackColor = true;
+            this.btnInvoiceUpdate.Click += new System.EventHandler(this.btnInvoiceUpdate_Click);
+            // 
+            // btnInvoiceCreate
+            // 
+            this.btnInvoiceCreate.Location = new System.Drawing.Point(799, 218);
+            this.btnInvoiceCreate.Name = "btnInvoiceCreate";
+            this.btnInvoiceCreate.Size = new System.Drawing.Size(110, 30);
+            this.btnInvoiceCreate.TabIndex = 8;
+            this.btnInvoiceCreate.Text = "Ajouter une facture";
+            this.btnInvoiceCreate.UseVisualStyleBackColor = true;
+            this.btnInvoiceCreate.Click += new System.EventHandler(this.btnInvoiceCreate_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(799, 282);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(110, 30);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(799, 339);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(110, 30);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(799, 407);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(110, 30);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(440, 470);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(110, 30);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btnInvoiceCreate);
+            this.Controls.Add(this.btnInvoiceUpdate);
+            this.Controls.Add(this.btnInvoiceList);
+            this.Controls.Add(this.btnProductCreate);
+            this.Controls.Add(this.btnProductUpdate);
+            this.Controls.Add(this.btnProductList);
+            this.Controls.Add(this.btnClientCreate);
+            this.Controls.Add(this.btnClientUpdate);
+            this.Controls.Add(this.btnClientList);
+            this.Name = "FormMenu";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoFact";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClientList;
+        private System.Windows.Forms.Button btnClientUpdate;
+        private System.Windows.Forms.Button btnClientCreate;
+        private System.Windows.Forms.Button btnProductList;
+        private System.Windows.Forms.Button btnProductUpdate;
+        private System.Windows.Forms.Button btnProductCreate;
+        private System.Windows.Forms.Button btnInvoiceList;
+        private System.Windows.Forms.Button btnInvoiceUpdate;
+        private System.Windows.Forms.Button btnInvoiceCreate;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ FormMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoFact2
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClientList_Click(object sender, EventArgs e)
+        {
+            FormClientList ClientList = new FormClientList();
+            ClientList.Show();
+        }
+
+        private void btnClientUpdate_Click(object sender, EventArgs e)
+        {
+            FormClientUpdate ClientUpdate = new FormClientUpdate();
+            ClientUpdate.Show();
+        }
+
+        private void btnClientCreate_Click(object sender, EventArgs e)
+        {
+            FormClientCreate ClientCreate = new FormClientCreate();
+            ClientCreate.Show();
+        }
+
+        private void btnProductList_Click(object sender, EventArgs e)
+        {
+            FormProductList ProductList = new FormProductList();
+            ProductList.Show();
+        }
+
+        private void btnProductUpdate_Click(object sender, EventArgs e)
+        {
+            FormProductUpdate ProductUpdate = new FormProductUpdate();
+            ProductUpdate.Show();
+        }
+
+        private void btnProductCreate_Click(object sender, EventArgs e)
+        {
+            FormProductCreate ProductCreate = new FormProductCreate();
+            ProductCreate.Show();
+        }
+
+        private void btnInvoiceList_Click(object sender, EventArgs e)
+        {
+            FormInvoiceList InvoiceList = new FormInvoiceList();
+            InvoiceList.Show();
+        }
+
+        private void btnInvoiceUpdate_Click(object sender, EventArgs e)
+        {
+            FormInvoiceUpdate InvoiceUpdate = new FormInvoiceUpdate();
+            InvoiceUpdate.Show();
+        }
+
+        private void btnInvoiceCreate_Click(object sender, EventArgs e)
+        {
+            FormInvoiceCreate InvoiceCreate = new FormInvoiceCreate();
+            InvoiceCreate.Show();
+        }
+    }
+}
