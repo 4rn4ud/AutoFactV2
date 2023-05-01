0f19e49781439a59efd512a92776b0a594b5dff4
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-02 00:48:03 +0200

    Create Type.cs

---
+++ Views/Type.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFact2.Views
+{
+    class Type
+    {
+        private int _id;
+        private string _libel;
+
+        public Type(int id, string libel)
+        {
+            this._id = id;
+            this._libel = libel;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void SetId(int value)
+        {
+            this._id = value;
+        }
+
+        public string GetLibel()
+        {
+            return _libel;
+        }
+
+        public void SetLibel(string value)
+        {
+            this._libel = value;
+        }
+    }
+}
