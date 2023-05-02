f8bcf48507ea8246cac19a7901e57815b1f1dddd
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2023-05-02 11:59:29 +0200

    Update FormClientCreate.Designer.cs

--- Views/FormClientCreate.Designer.cs
+++ Views/FormClientCreate.Designer.cs
@@ -28,21 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnBack = new System.Windows.Forms.Button();
+            this.BtnCreate = new System.Windows.Forms.Button();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LabelName = new System.Windows.Forms.Label();
+            this.LabelSurname = new System.Windows.Forms.Label();
+            this.LabelCompagny = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // BtnBack
+            // 
+            this.BtnBack.Location = new System.Drawing.Point(52, 22);
+            this.BtnBack.Name = "BtnBack";
+            this.BtnBack.Size = new System.Drawing.Size(75, 23);
+            this.BtnBack.TabIndex = 0;
+            this.BtnBack.Text = "Retour";
+            this.BtnBack.UseVisualStyleBackColor = true;
+            // 
+            // BtnCreate
+            // 
+            this.BtnCreate.Location = new System.Drawing.Point(419, 460);
+            this.BtnCreate.Name = "BtnCreate";
+            this.BtnCreate.Size = new System.Drawing.Size(75, 23);
+            this.BtnCreate.TabIndex = 1;
+            this.BtnCreate.Text = "Enregistrer";
+            this.BtnCreate.UseVisualStyleBackColor = true;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(149, 144);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(100, 20);
+            this.TxtName.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 206);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 267);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(409, 144);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(409, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(409, 268);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(667, 144);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 8;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(667, 206);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(314, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            // 
+            // LabelName
+            // 
+            this.LabelName.AutoSize = true;
+            this.LabelName.Location = new System.Drawing.Point(52, 143);
+            this.LabelName.Name = "LabelName";
+            this.LabelName.Size = new System.Drawing.Size(43, 13);
+            this.LabelName.TabIndex = 17;
+            this.LabelName.Text = "Prenom";
+            // 
+            // LabelSurname
+            // 
+            this.LabelSurname.AutoSize = true;
+            this.LabelSurname.Location = new System.Drawing.Point(52, 205);
+            this.LabelSurname.Name = "LabelSurname";
+            this.LabelSurname.Size = new System.Drawing.Size(29, 13);
+            this.LabelSurname.TabIndex = 18;
+            this.LabelSurname.Text = "Nom";
+            // 
+            // LabelCompagny
+            // 
+            this.LabelCompagny.AutoSize = true;
+            this.LabelCompagny.Location = new System.Drawing.Point(52, 267);
+            this.LabelCompagny.Name = "LabelCompagny";
+            this.LabelCompagny.Size = new System.Drawing.Size(35, 13);
+            this.LabelCompagny.TabIndex = 19;
+            this.LabelCompagny.Text = "label9";
             // 
             // FormClientCreate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.LabelCompagny);
+            this.Controls.Add(this.LabelSurname);
+            this.Controls.Add(this.LabelName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.BtnCreate);
+            this.Controls.Add(this.BtnBack);
             this.Name = "FormClientCreate";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AutoFact";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnBack;
+        private System.Windows.Forms.Button BtnCreate;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LabelName;
+        private System.Windows.Forms.Label LabelSurname;
+        private System.Windows.Forms.Label LabelCompagny;
     }
 }
