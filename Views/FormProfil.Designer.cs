092d98e9a0c742ae3a579ad6d849323627b59c33
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-02 12:24:26 +0200

    Update FormProfil.Designer.cs

--- Views/FormProfil.Designer.cs
+++ Views/FormProfil.Designer.cs
@@ -28,21 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRaisonSocial = new System.Windows.Forms.TextBox();
+            this.txtSiren = new System.Windows.Forms.TextBox();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.txtCP = new System.Windows.Forms.TextBox();
+            this.txtVille = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(358, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":Raison social";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":Numéro de siren";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ":Adresse";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(358, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":Code postal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(358, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ":Ville";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(358, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ":Numéro de téléphone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(358, 335);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = ":E-mail";
+            // 
+            // txtRaisonSocial
+            // 
+            this.txtRaisonSocial.Location = new System.Drawing.Point(539, 125);
+            this.txtRaisonSocial.Name = "txtRaisonSocial";
+            this.txtRaisonSocial.ReadOnly = true;
+            this.txtRaisonSocial.Size = new System.Drawing.Size(100, 20);
+            this.txtRaisonSocial.TabIndex = 7;
+            // 
+            // txtSiren
+            // 
+            this.txtSiren.Location = new System.Drawing.Point(539, 165);
+            this.txtSiren.Name = "txtSiren";
+            this.txtSiren.ReadOnly = true;
+            this.txtSiren.Size = new System.Drawing.Size(100, 20);
+            this.txtSiren.TabIndex = 8;
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(539, 200);
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.ReadOnly = true;
+            this.txtAdresse.Size = new System.Drawing.Size(100, 20);
+            this.txtAdresse.TabIndex = 9;
+            // 
+            // txtCP
+            // 
+            this.txtCP.Location = new System.Drawing.Point(539, 232);
+            this.txtCP.Name = "txtCP";
+            this.txtCP.ReadOnly = true;
+            this.txtCP.Size = new System.Drawing.Size(100, 20);
+            this.txtCP.TabIndex = 10;
+            // 
+            // txtVille
+            // 
+            this.txtVille.Location = new System.Drawing.Point(539, 261);
+            this.txtVille.Name = "txtVille";
+            this.txtVille.ReadOnly = true;
+            this.txtVille.Size = new System.Drawing.Size(100, 20);
+            this.txtVille.TabIndex = 11;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(539, 293);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.ReadOnly = true;
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(539, 332);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ReadOnly = true;
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 13;
             // 
             // FormProfil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtVille);
+            this.Controls.Add(this.txtCP);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.txtSiren);
+            this.Controls.Add(this.txtRaisonSocial);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FormProfil";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AutoFact";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtRaisonSocial;
+        private System.Windows.Forms.TextBox txtSiren;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.TextBox txtCP;
+        private System.Windows.Forms.TextBox txtVille;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
