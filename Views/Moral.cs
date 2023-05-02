14bbefa67bd8a61a43643a48df55c96f1d3ca5db
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-02 08:58:09 +0200

    Create Moral.cs

---
+++ Views/Moral.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFact2.Views
+{
+    class Moral:Customer
+    {
+        private string _companyName;
+
+        public Moral(int id, string companyName, string address, string postalCode, string city, string mail, string tel) : base(id, address, postalCode, city, mail, tel)
+        {
+            this._companyName = companyName;
+        }
+
+        public string GetCompanyName()
+        {
+            return _companyName;
+        }
+
+        public void SetCompanyName(string value)
+        {
+            this._companyName = value;
+        }
+    }
+}
