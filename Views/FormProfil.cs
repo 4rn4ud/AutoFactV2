deafbbaa147bef003a55a1586e65d6d4437e8c02
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-04 11:54:09 +0200

    Update FormProfil.cs

--- Views/FormProfil.cs
+++ Views/FormProfil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,8 +20,30 @@ namespace AutoFact2.Views
 
         private void FormProfil_Load(object sender, EventArgs e)
         {
+            string connectionString = "Data Source=../../AutoFact2BDD.db";
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
 
-            txtEmail.Text = GetMail();
+            connection.Open();
+
+            string selectSql = "SELECT * FROM Profil";
+            SQLiteCommand command = new SQLiteCommand(selectSql, connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                txtRaisonSocial.Text = reader.GetString(1);
+                txtSiren.Text = reader.GetInt32(2).ToString();
+                txtAdresse.Text = reader.GetString(3);
+                txtCP.Text = reader.GetInt32(4).ToString();
+                txtVille.Text = reader.GetString(5);
+                txtTel.Text = reader.GetString(6);
+                txtEmail.Text = reader.GetString(7);
+            }
+
+            reader.Close();
+
+            connection.Close();
+
         }
     }
 }
