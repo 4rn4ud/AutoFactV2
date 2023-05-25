f747abb82621748d234e540c8b9640ef956ea54d
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-25 22:08:48 +0200

    create FormClientUpdate

---
+++ Views/FormClientUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoFact2.Views
+{
+    public partial class FormClientUpdate : Form
+    {
+        public FormClientUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FormClientUpdate_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
