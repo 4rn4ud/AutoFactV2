d9d21df242cd4cdbb456639e18c60b3e59be8382
4rn4ud <arnaud.lechat1@gmail.com>
2023-05-25 11:25:55 +0200

    changes

--- Models/Moral.cs
+++ Models/Moral.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks;
 
 namespace AutoFact2.Views
 {
-    class Moral:Customer
+    public class Moral:Customer
     {
         private string _companyName;
 
-        public Moral(int id, string companyName, string address, string postalCode, string city, string mail, string tel) : base(id, address, postalCode, city, mail, tel)
+        public Moral(int id, string companyName, string address, int postalCode, string city, string mail, string tel) : base(id, address, postalCode, city, mail, tel)
         {
             this._companyName = companyName;
         }
 
-        public string GetCompanyName()
+        public override string GetCompanyName()
         {
+            base.GetCompanyName();
             return _companyName;
         }
 
--- Models/Physical.cs
+++ Models/Physical.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 
 namespace AutoFact2.Views
 {
-    class Physical:Customer
+    public class Physical:Customer
     {
         private string _name;
         private string _lastname;
 
-        public Physical(int id, string name, string lastname, string address, string postalCode, string city, string mail, string tel) : base(id, address, postalCode, city, mail, tel)
+        public Physical(int id, string name, string lastname, string address, int postalCode, string city, string mail, string tel) : base(id, address, postalCode, city, mail, tel)
         {
             this._name = name;
             this._lastname = lastname;
         }
 
-        public string GetName()
+        public override string GetName()
         {
+            base.GetName();
             return _name;
         }
 
@@ -27,8 +28,9 @@ namespace AutoFact2.Views
             this._name = value;
         }
 
-        public string GetLastname()
+        public override string GetLastname()
         {
+            base.GetLastname();
             return _lastname;
         }
 
