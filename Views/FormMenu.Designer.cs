e009b3892f39f183716d3eaee005b03b7190c18d
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2023-06-02 22:57:39 +0200

    deco

--- Views/FormMenu.Designer.cs
+++ Views/FormMenu.Designer.cs
@@ -34,12 +34,15 @@
             this.btnProfil = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LabelTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnClientList
             // 
             this.btnClientList.Location = new System.Drawing.Point(125, 107);
-            this.btnClientList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClientList.Margin = new System.Windows.Forms.Padding(4);
             this.btnClientList.Name = "btnClientList";
             this.btnClientList.Size = new System.Drawing.Size(147, 37);
             this.btnClientList.TabIndex = 0;
@@ -49,8 +52,8 @@
             // 
             // btnProductList
             // 
-            this.btnProductList.Location = new System.Drawing.Point(125, 296);
-            this.btnProductList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnProductList.Location = new System.Drawing.Point(125, 201);
+            this.btnProductList.Margin = new System.Windows.Forms.Padding(4);
             this.btnProductList.Name = "btnProductList";
             this.btnProductList.Size = new System.Drawing.Size(147, 37);
             this.btnProductList.TabIndex = 3;
@@ -60,8 +63,8 @@
             // 
             // btnInvoiceList
             // 
-            this.btnInvoiceList.Location = new System.Drawing.Point(1065, 122);
-            this.btnInvoiceList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInvoiceList.Location = new System.Drawing.Point(933, 107);
+            this.btnInvoiceList.Margin = new System.Windows.Forms.Padding(4);
             this.btnInvoiceList.Name = "btnInvoiceList";
             this.btnInvoiceList.Size = new System.Drawing.Size(147, 37);
             this.btnInvoiceList.TabIndex = 6;
@@ -71,8 +74,8 @@
             // 
             // btnProfil
             // 
-            this.btnProfil.Location = new System.Drawing.Point(1065, 296);
-            this.btnProfil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnProfil.Location = new System.Drawing.Point(933, 201);
+            this.btnProfil.Margin = new System.Windows.Forms.Padding(4);
             this.btnProfil.Name = "btnProfil";
             this.btnProfil.Size = new System.Drawing.Size(147, 37);
             this.btnProfil.TabIndex = 9;
@@ -82,8 +85,8 @@
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(1065, 501);
-            this.button12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button12.Location = new System.Drawing.Point(933, 296);
+            this.button12.Margin = new System.Windows.Forms.Padding(4);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(147, 37);
             this.button12.TabIndex = 11;
@@ -92,30 +95,51 @@
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(587, 578);
-            this.button13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button13.Location = new System.Drawing.Point(125, 296);
+            this.button13.Margin = new System.Windows.Forms.Padding(4);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(147, 37);
             this.button13.TabIndex = 12;
             this.button13.Text = "button13";
             this.button13.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(400, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LabelTitle
+            // 
+            this.LabelTitle.Location = new System.Drawing.Point(452, 19);
+            this.LabelTitle.Name = "LabelTitle";
+            this.LabelTitle.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LabelTitle.Size = new System.Drawing.Size(300, 50);
+            this.LabelTitle.TabIndex = 14;
+            this.LabelTitle.Text = "AutoFact2";
+            this.LabelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1312, 690);
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.LabelTitle);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.btnProfil);
             this.Controls.Add(this.btnInvoiceList);
             this.Controls.Add(this.btnProductList);
             this.Controls.Add(this.btnClientList);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormMenu";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AutoFact";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +152,8 @@
         private System.Windows.Forms.Button btnProfil;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LabelTitle;
     }
 }
 
