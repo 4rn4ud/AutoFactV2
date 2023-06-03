ce7f6522b3dc93478d93f644adf873ac9d46562c
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2023-06-03 11:34:48 +0200

    regex update

--- Views/FormClientUpdate.cs
+++ Views/FormClientUpdate.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,6 +20,8 @@ namespace AutoFact2.Views
         {
             InitializeComponent();
             customerController = new CustomerController();
+
+
 
             foreach (var unClient in customerController.findAll())
             { 
@@ -39,6 +42,12 @@ namespace AutoFact2.Views
 
                 }
             }
+
+            TxtMail.TextChanged += TxtMail_TextChanged;
+            TxtPostalCode.TextChanged += TxtPostalCode_TextChanged;
+            TxtTel.TextChanged += TxtTel_TextChanged;
+            TxtSurname.TextChanged += TxtSurname_TextChanged;
+            TxtName.TextChanged += TxtName_TextChanged;
         }
 
         private void BtnBack_Click(object sender, EventArgs e)
@@ -59,16 +68,171 @@ namespace AutoFact2.Views
             string lastName = TxtSurname.Text;
             string companyName = TxtCompanyname.Text;
             string address = TxtAdresse.Text;
-            int postalCode = Convert.ToInt32(TxtPostalCode.Text);
+            string postalCode = TxtPostalCode.Text;
             string city = TxtCity.Text;
             string mail = TxtMail.Text;
             string tel = TxtTel.Text;
-            
 
-            customerController.update(id, name, lastName, companyName, address, postalCode, city, mail, tel);
-            //int id, string name, string lastName, string companyName, string address, int postalCode, string city, string mail, string tel
-            MessageBox.Show("Le client a bien été Modifier dans la base de données.");
-            this.Close();
+            string namePattern = @"^[a-zA-Z]+$";
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            string telPattern = @"^(0|\+33)\d{9}$";
+            string postalCodePattern = @"^\d{5}$";
+            string surnamePattern = @"^[a-zA-Z]+$";
+
+            bool isValid = true;
+            string errorMessage = "";
+
+            if (!Regex.IsMatch(name, namePattern))
+            {
+                isValid = false;
+                errorMessage += "Le nom est invalide. Il ne doit contenir que des lettres.\n";
+            }
+
+            if (!Regex.IsMatch(mail, emailPattern))
+            {
+                isValid = false;
+                errorMessage += "L'email est invalide. Veuillez saisir une adresse email valide.\n";
+            }
+
+            if (!Regex.IsMatch(tel, telPattern))
+            {
+                isValid = false;
+                errorMessage += "Le numéro de téléphone est invalide. Veuillez saisir un numéro de téléphone valide.\n";
+            }
+
+            if (!Regex.IsMatch(postalCode, postalCodePattern))
+            {
+                isValid = false;
+                errorMessage += "Le code postal est invalide. Veuillez saisir un code postal valide (5 chiffres).\n";
+            }
+
+            if (!Regex.IsMatch(lastName, surnamePattern))
+            {
+                isValid = false;
+                errorMessage += "Le nom de famille est invalide. Il ne doit contenir que des lettres.\n";
+            }
+
+            if (!isValid)
+            {
+                MessageBox.Show(errorMessage, "Erreurs de validation");
+                return;
+            }
+
+            if (isValid)
+            {
+                customerController.update(id, name, lastName, companyName, address, Convert.ToInt32(postalCode), city, mail, tel);
+                //int id, string name, string lastName, string companyName, string address, int postalCode, string city, string mail, string tel
+                MessageBox.Show("Le client a bien été Modifier dans la base de données.");
+                this.Close();
+            }
+
+
+
         }
+
+        private void TxtMail_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string email = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format de l'adresse e-mail
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            bool isValidEmail = Regex.IsMatch(email, emailPattern);
+
+            if (isValidEmail)
+            {
+                // L'adresse e-mail est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // L'adresse e-mail est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void TxtPostalCode_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string postalCode = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du code postal (exemple pour la France)
+            string postalCodePattern = @"^\d{5}$";
+            bool isValidPostalCode = Regex.IsMatch(postalCode, postalCodePattern);
+
+            if (isValidPostalCode)
+            {
+                // Le code postal est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le code postal est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+        private void TxtTel_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string tel = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du numéro de téléphone (exemple pour les numéros français)
+            string telPattern = @"^(0|\+33)\d{9}$";
+            bool isValidTel = Regex.IsMatch(tel, telPattern);
+
+            if (isValidTel)
+            {
+                // Le numéro de téléphone est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le numéro de téléphone est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void TxtName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string tel = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du numéro de téléphone (exemple pour les numéros français)
+            string namePattern = @"^[a-zA-Z]+$";
+            bool isValidTel = Regex.IsMatch(tel, namePattern);
+
+            if (isValidTel)
+            {
+                // Le numéro de téléphone est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le numéro de téléphone est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void TxtSurname_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string tel = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du numéro de téléphone (exemple pour les numéros français)
+            string surnamePattern = @"^[a-zA-Z]+$";
+            bool isValidTel = Regex.IsMatch(tel, surnamePattern);
+
+            if (isValidTel)
+            {
+                // Le numéro de téléphone est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le numéro de téléphone est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
     }
 }
