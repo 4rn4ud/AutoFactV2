edd77101ec6856ceae77b3a56045f3c3b1381726
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2023-06-03 10:55:39 +0200

    regex

--- Views/FormClientCreate.cs
+++ Views/FormClientCreate.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace AutoFact2.Views
 {
@@ -18,6 +19,9 @@ namespace AutoFact2.Views
         {
             InitializeComponent();
             customerController = new CustomerController();
+            TxtMail.TextChanged += TxtMail_TextChanged;
+            TxtPostalCode.TextChanged += TxtPostalCode_TextChanged;
+            TxtTel.TextChanged += TxtTel_TextChanged;
         }
 
         private void BtnBack_Click(object sender, EventArgs e)
@@ -27,6 +31,7 @@ namespace AutoFact2.Views
 
         private void BtnCreate_Click(object sender, EventArgs e)
         {
+
             string name = TxtName.Text;
             string lastName = TxtSurname.Text;
             string companyName = TxtCompanyname.Text;
@@ -55,5 +60,68 @@ namespace AutoFact2.Views
             TxtMail.Text = string.Empty;
             TxtTel.Text = string.Empty;
         }
+
+        private void TxtMail_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string email = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format de l'adresse e-mail
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            bool isValidEmail = Regex.IsMatch(email, emailPattern);
+
+            if (isValidEmail)
+            {
+                // L'adresse e-mail est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // L'adresse e-mail est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void TxtPostalCode_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string postalCode = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du code postal (exemple pour la France)
+            string postalCodePattern = @"^\d{5}$";
+            bool isValidPostalCode = Regex.IsMatch(postalCode, postalCodePattern);
+
+            if (isValidPostalCode)
+            {
+                // Le code postal est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le code postal est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+        private void TxtTel_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string tel = textBox.Text.Trim();
+
+            // Expression régulière pour valider le format du numéro de téléphone (exemple pour les numéros français)
+            string telPattern = @"^(0|\+33)\d{9}$";
+            bool isValidTel = Regex.IsMatch(tel, telPattern);
+
+            if (isValidTel)
+            {
+                // Le numéro de téléphone est valide
+                textBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                // Le numéro de téléphone est invalide
+                textBox.ForeColor = Color.Red;
+            }
+        }
+
     }
 }
